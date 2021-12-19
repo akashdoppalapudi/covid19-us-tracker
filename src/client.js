5eed63668a17f209a0bd4b5fc7f214ba2416bed7
Akash Doppalapudi <akashdoppalapudi2001@gmail.com>
2021-12-19 12:29:36 +0530

    did titles for all pages and done about page

--- src/client.js
+++ src/client.js
@@ -1,5 +1,6 @@
 import * as sapper from '@sapper/app';
 import '../node_modules/bulma/css/bulma.min.css';
+import '../static/global.css';
 
 sapper.start({
 	target: document.querySelector('#sapper'),
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,1 +1,12 @@
-<h1>About</h1>
+<svelte:head>
+    <title>About Covid 19 Tracker US</title>
+</svelte:head>
+
+<div class="section header">
+    <div class="container">
+        <h1>About</h1>
+        <p>
+            The data from this site came from <a href="https://covidtracking.com">The Covid Tracking Project</a>. You can learn more about going to their site
+        </p>
+    </div>
+</div>
